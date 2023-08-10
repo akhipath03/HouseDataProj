dfbf80efd4526a18689dbfa55a166f12b695ceaa
sdharanikota6 <sdharanikota6@gatech.edu>
2023-08-09 22:51:01 -0400

    neural update and randomforest regressor

--- neural.py
+++ neural.py
@@ -6,7 +6,7 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import tensorflow as tf
 from tensorflow import keras
 from keras import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 
 # Load the dataset
 df = pd.read_csv(r"/Users/sudeepdharanikota/Desktop/HouseDataProj/processeddata.csv")
@@ -28,33 +28,49 @@ X_scaled = scaler.fit_transform(X)
 # Split data into training and validation sets
 X_train, X_val, y_train, y_val = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
-# Initialize the model
+# Define the model architecture
 model = Sequential()
 
 # Input Layer
 model.add(Dense(64, input_dim=X_train.shape[1], activation='relu'))
+model.add(Dropout(0.2))
 
 # Hidden Layers
-model.add(Dense(128, activation='relu'))
-model.add(Dense(256, activation='relu'))
-model.add(Dense(128, activation='relu'))
-model.add(Dense(64, activation='relu'))
+model.add(Dense(128, activation='relu', kernel_regularizer=keras.regularizers.l2(0.001)))
+model.add(Dropout(0.2))
+
+model.add(Dense(128, activation='relu', kernel_regularizer=keras.regularizers.l2(0.001)))
+model.add(Dropout(0.2))
+
+model.add(Dense(64, activation='relu', kernel_regularizer=keras.regularizers.l2(0.001)))
+model.add(Dropout(0.2))
 
 # Output Layer
 model.add(Dense(1, activation='linear'))
 
-# Compile the model
-model.compile(optimizer='adam', loss='mean_squared_error')
+# Compile the model with Adam optimizer
+model.compile(optimizer=keras.optimizers. Adam(learning_rate=1e-3), loss='mean_squared_error')
+
+# Additional Callbacks
+early_stopping = keras.callbacks.EarlyStopping(patience=10, restore_best_weights=True)
+reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, verbose=1)
 
 # Train the model
-history = model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=100, batch_size=10)
+model.fit(X_train, y_train, validation_data=(X_val, y_val), 
+          epochs=100, batch_size=32, 
+          callbacks=[early_stopping, reduce_lr])
 
 # Predict on the validation set
 y_pred = model.predict(X_val)
 
-# Calculate r^2
+# Calculate evaluation metrics
+mae = mean_absolute_error(y_val, y_pred)
+mse = mean_squared_error(y_val, y_pred)
 r2 = r2_score(y_val, y_pred)
-print(f"R-squared: {r2}")
+
+print(f"Modified Model - Mean Absolute Error: {mae}")
+print(f"Modified Model - Mean Squared Error: {mse}")
+print(f"Modified Model - R-squared: {r2}")
 
 # Make a prediction
 sample_input = np.array([X_val[0]])  # Replace with your input data
---
+++ randomforest.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+# Load the dataset
+df = pd.read_csv(r"/Users/sudeepdharanikota/Desktop/HouseDataProj/processeddata.csv")
+
+# Split features and target
+X = df.drop(columns=['price'])
+y = df['price']
+
+# One hot encode zip_code
+encoder = OneHotEncoder()
+encoded_zip = encoder.fit_transform(X['zip_code'].values.reshape(-1, 1)).toarray()
+encoded_zip_df = pd.DataFrame(encoded_zip, columns=[f'zip_{cat}' for cat in encoder.categories_[0]])
+X = pd.concat([X.drop(columns='zip_code'), encoded_zip_df], axis=1)
+
+# Normalize other features
+scaler = MinMaxScaler()
+X_scaled = scaler.fit_transform(X)
+
+# Split data into training and validation sets
+X_train, X_val, y_train, y_val = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
+# Define the hyperparameters and their possible values for tuning
+# Define the hyperparameters and their possible values for tuning
+param_dist = {
+    'n_estimators': [100, 200, 500, 1000],
+    'max_features': ['sqrt', 'log2'],
+    'max_depth': [None, 10, 20, 30, 40, 50],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4],
+    'bootstrap': [True, False]
+}
+
+
+# Initialize the RandomForestRegressor
+rf_regressor = RandomForestRegressor(random_state=42, n_jobs=-1)
+
+# Use RandomizedSearchCV to search for best hyperparameters
+rf_search = RandomizedSearchCV(estimator=rf_regressor, param_distributions=param_dist, 
+                               n_iter=100, cv=5, verbose=2, random_state=42, n_jobs=-1)
+
+# Fit the random search model
+rf_search.fit(X_train, y_train)
+
+# Print the best parameters
+print("Best Parameters from RandomizedSearchCV: ", rf_search.best_params_)
+
+# Use the best estimator from RandomizedSearchCV to make predictions
+best_rf = rf_search.best_estimator_
+y_pred_rf = best_rf.predict(X_val)
+
+# Calculate evaluation metrics
+mae_rf = mean_absolute_error(y_val, y_pred_rf)
+mse_rf = mean_squared_error(y_val, y_pred_rf)
+r2_rf = r2_score(y_val, y_pred_rf)
+
+print(f"Random Forest - Mean Absolute Error: {mae_rf}")
+print(f"Random Forest - Mean Squared Error: {mse_rf}")
+print(f"Random Forest - R-squared: {r2_rf}")
+
+# Make a prediction using the best RandomForestRegressor
+sample_input_rf = X_val[0].reshape(1, -1)
+predicted_price_rf = best_rf.predict(sample_input_rf)
+print(f"Predicted Price using Random Forest: {predicted_price_rf[0]}")
